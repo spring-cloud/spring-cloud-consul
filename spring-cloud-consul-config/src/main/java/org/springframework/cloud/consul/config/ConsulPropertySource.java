2c9cebfb7fbf4c79daf45ff2a65a970f85518537
Zach Marshall <Zach.Marshall@sas.com>
2015-05-14 18:08:37 -0400

    Don't add properties for consul folders
    The Consul KeyValue client returns folder names as KeyValue pairs
    with null values. For example, if there is a config/application/ldap/
    folder, then the KeyValue client returns this as a KeyValue and then
    spring-cloud-consul adds this as a property, eg
    config.application.ldap.=null. This is not a real property and
    shouldn't be added to the environment. Specifically, this causes the
    bean binding phase to fail since it can think it has a property with
    a zero-length name if it finds a @ConfigurationProperties bean with
    the prefix ldap (for example).

--- spring-cloud-consul-config/src/main/java/org/springframework/cloud/consul/config/ConsulPropertySource.java
+++ spring-cloud-consul-config/src/main/java/org/springframework/cloud/consul/config/ConsulPropertySource.java
@@ -1,79 +1,57 @@
-/*
- * Copyright 2013-2015 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.springframework.cloud.consul.config;
 
-import static org.springframework.util.Base64Utils.decodeFromString;
-
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-
+import org.springframework.cloud.consul.client.KeyValueClient;
+import org.springframework.cloud.consul.client.NotFoundException;
+import org.springframework.cloud.consul.model.KeyValue;
 import org.springframework.core.env.EnumerablePropertySource;
 
-import com.ecwid.consul.v1.ConsulClient;
-import com.ecwid.consul.v1.QueryParams;
-import com.ecwid.consul.v1.Response;
-import com.ecwid.consul.v1.kv.model.GetValue;
+import java.util.*;
 
 /**
  * @author Spencer Gibb
  */
-public class ConsulPropertySource extends EnumerablePropertySource<ConsulClient> {
+public class ConsulPropertySource extends EnumerablePropertySource<KeyValueClient> {
 
-	private String context;
+    private String context;
 
-	private Map<String, String> properties = new LinkedHashMap<>();
+    private Map<String, String> properties = new LinkedHashMap<>();
 
-	public ConsulPropertySource(String context, ConsulClient source) {
-		super(context, source);
-		this.context = context;
+    public ConsulPropertySource(String context, KeyValueClient source) {
+        super(context, source);
+        this.context = context;
 
-		if (!this.context.endsWith("/")) {
-			this.context = this.context + "/";
-		}
-	}
+        if (!this.context.endsWith("/")) {
+            this.context = this.context + "/";
+        }
+    }
 
-	public void init() {
-		Response<List<GetValue>> response = source.getKVValues(context,
-				QueryParams.DEFAULT);
-		List<GetValue> values = response.getValue();
+    public void init() {
+        try {
+            List<KeyValue> keyValues = source.getKeyValueRecurse(context);
 
-		if (values != null) {
-			for (GetValue getValue : values) {
-				String key = getValue.getKey().replace(context, "").replace('/', '.');
-				String value = getDecoded(getValue.getValue());
-				properties.put(key, value);
-			}
-		}
-	}
+            for (KeyValue keyValue : keyValues) {
+                if (null != keyValue.getKey() && keyValue.getKey().endsWith("/")) {
+                    // don't add folders as properties, this causes errors during binding
+                    continue;
+                }
+                String key = keyValue.getKey()
+                    .replace(context, "")
+                    .replace('/', '.');
+                String value = keyValue.getDecoded();
+                properties.put(key, value);
+            }
+        } catch (NotFoundException e) {
+            //not found, do nothing
+        }
+    }
 
-	public String getDecoded(String value) {
-		if (value == null)
-			return null;
-		return new String(decodeFromString(value));
-	}
+    @Override
+    public Object getProperty(String name) {
+        return properties.get(name);
+    }
 
-	@Override
-	public Object getProperty(String name) {
-		return properties.get(name);
-	}
-
-	@Override
-	public String[] getPropertyNames() {
-		return properties.keySet().toArray(new String[0]);
-	}
+    @Override
+    public String[] getPropertyNames() {
+        return properties.keySet().toArray(new String[0]);
+    }
 }
