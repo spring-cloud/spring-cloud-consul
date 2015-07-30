2446ae6aaf193b2b06a29a472fffd8037c1980d2
andepo <andrew.depompa@sas.com>
2015-07-30 11:39:30 -0400

    Adds default configuration listener and updates tests to handle odd changes brought about by existence of listener

--- spring-cloud-consul-config/src/main/java/org/springframework/cloud/consul/config/ConsulConfigApplicationConfiguration.java
+++ spring-cloud-consul-config/src/main/java/org/springframework/cloud/consul/config/ConsulConfigApplicationConfiguration.java
@@ -3,7 +3,9 @@ package org.springframework.cloud.consul.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.bootstrap.config.RefreshEndpoint;
 import org.springframework.cloud.consul.ConsulAutoConfiguration;
+import org.springframework.cloud.consul.config.ConsulConfigurationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -13,15 +15,22 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @Import(ConsulAutoConfiguration.class)
 @EnableConfigurationProperties
 @EnableScheduling
-@ConditionalOnProperty(name = "spring.cloud.consul.config.enabled", matchIfMissing = true)
+@ConditionalOnProperty(name = "spring.cloud.consul.config.watch", matchIfMissing = false)
 public class ConsulConfigApplicationConfiguration {
 
 	@Autowired
 	ConsulConfigProperties consulConfigProperties;
+	
+	@Autowired
+    private RefreshEndpoint refreshEndpoint;
 
 	@Bean
-	@ConditionalOnProperty(name = "spring.cloud.consul.config.watch", matchIfMissing = false)
 	public ConsulConfigWatch consulConfigWatch() {
 		return new ConsulConfigWatch(consulConfigProperties);
 	}
+	
+    @Bean
+    public ConsulConfigurationListener consulConfigurationListener() {
+        return new ConsulConfigurationListener(refreshEndpoint);
+    }
 }
