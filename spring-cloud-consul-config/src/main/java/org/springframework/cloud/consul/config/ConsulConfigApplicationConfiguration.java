f77e6e868a56f7e14e553bb7a2d951620771cf58
andepo <andrew.depompa@sas.com>
2015-08-05 14:30:53 -0400

    More merge fallout plus minor test update

--- spring-cloud-consul-config/src/main/java/org/springframework/cloud/consul/config/ConsulConfigApplicationConfiguration.java
+++ spring-cloud-consul-config/src/main/java/org/springframework/cloud/consul/config/ConsulConfigApplicationConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.bootstrap.config.RefreshEndpoint;
 import org.springframework.cloud.consul.ConsulAutoConfiguration;
-import org.springframework.cloud.consul.config.ConsulConfigurationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
