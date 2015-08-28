5272a931281e38cac8eb877c3ef6cd1c25889828
Spencer Gibb <spencer@gibb.us>
2015-08-28 13:09:20 -0600

    polish

--- spring-cloud-vault-config/src/main/java/org/springframework/cloud/vault/config/VaultClient.java
+++ spring-cloud-vault-config/src/main/java/org/springframework/cloud/vault/config/VaultClient.java
@@ -22,15 +22,12 @@ import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpStatusCodeException;
-import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 /**
