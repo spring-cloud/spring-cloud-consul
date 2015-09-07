7ad66e9b0e95e247c3a502c6ffadf1fae67e256a
Bill Gray <bill.gray@level3.com>
2015-09-06 23:01:06 -0600

    fix sleep in travis and local_run_consul scripts

--- src/main/bash/local_run_consul.sh
+++ src/main/bash/local_run_consul.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 consul agent -server -bootstrap-expect 1 -data-dir /tmp/consul -ui-dir `dirname $0`/../../../src/test/resources/consul_ui -config-dir `dirname $0`/../../test/resources/consul_config &
 # wait for consul to elect a leader before sending acl
-sleep 5000
+sleep 5
 curl -X PUT -d @`dirname $0`/../../test/resources/consul_acl/consul_discovery_client_acl.json http://localhost:8500/v1/acl/create?token=2ee647bd-bd69-4118-9f34-b9a6e9e60746
