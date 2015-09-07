1e1d3c78645be48fde440ad9d65c7b25514cbc68
Bill Gray <bill.gray@level3.com>
2015-09-06 22:52:53 -0600

    add acl tests and setup test instance of consul to support acl testing

--- src/main/bash/local_run_consul.sh
+++ src/main/bash/local_run_consul.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 mkdir /tmp/consul-config
-consul agent -server -bootstrap-expect 1 -advertise 127.0.0.1 -data-dir /tmp/consul -config-dir=/tmp/consul-config -ui-dir `dirname $0`/../../../src/test/resources/consul_ui
+BASEDIR=`dirname $0`/../../..
+consul agent -server -bootstrap-expect 1 -advertise 127.0.0.1 -data-dir /tmp/consul -config-dir=/tmp/consul-config -ui-dir ${BASEDIR}/src/test/resources/consul_ui
