1b31908eb428abb6d27180b549e636376f95e711
Srikalyan Swayampakula <srikalyan.swayampakula@locationlabs.com>
2015-11-18 16:56:42 -0800

    Support different formats for config.
    Add support for YAML and Properties blobs.
    Key/Value is still the default.

--- spring-cloud-consul-config/src/main/java/org/springframework/cloud/consul/config/ConsulConfigProperties.java
+++ spring-cloud-consul-config/src/main/java/org/springframework/cloud/consul/config/ConsulConfigProperties.java
@@ -16,10 +16,10 @@
 
 package org.springframework.cloud.consul.config;
 
-import lombok.Data;
-
 import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Data;
 
 /**
  * @author Spencer Gibb
@@ -38,5 +38,59 @@ public class ConsulConfigProperties {
 	@NotEmpty
 	private String profileSeparator = ",";
 
+	@NotEmpty
+	private Format format = Format.KEY_VALUE;
+
+	/**
+	 * If format is Format.PROPERTIES or Format.YAML
+	 * then the following field is used as key to look up consul for configuration.
+	 */
+	@NotEmpty
+	private String dataKey = "data";
+
 	private String aclToken;
+
+	/**
+	 * There are many ways in which we can specify configuration in consul i.e.,
+	 *
+	 * <ol>
+	 * <li>
+	 * Nested key value style: Where value is either a constant or part of the key (nested).
+	 * For e.g., For following configuration a.b.c=something a.b.d=something else One can
+	 * specify the configuration in consul with key as "../kv/config/application/a/b/c" and
+	 * value as "something" and key as "../kv/config/application/a/b/d" and value as
+	 * "something else"</li>
+	 * <li>
+	 * Entire contents of properties file as value For e.g., For following configuration
+	 * a.b.c=something a.b.d=something else One can specify the configuration in consul with
+	 * key as "../kv/config/application/properties" and value as whole configuration "
+	 * a.b.c=something a.b.d=something else "</li>
+	 * <li>
+	 * as Json or YML. You get it.</li>
+	 * </ol>
+	 *
+	 * This enum specifies the different Formats/styles supported for loading the
+	 * configuration.
+	 *
+	 * @author srikalyan.swayampakula
+	 */
+	public static enum Format {
+		/**
+		 * Indicates that the configuration specified in consul is of type native key values.
+		 */
+		KEY_VALUE,
+
+		/**
+		 * Indicates that the configuration specified in consul is of property style i.e.,
+		 * value of the consul key would be a list of key=value pairs separated by new lines.
+		 */
+		PROPERTIES,
+
+		/**
+		 * Indicates that the configuration specified in consul is of YAML style i.e., value
+		 * of the consul key would be YAML format
+		 */
+		YAML;
+
+	}
 }
---
+++ spring-cloud-consul-config/src/test/java/org/springframework/cloud/consul/config/ConsulPropertySourceTests.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2013-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.consul.config;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+
+import java.util.Random;
+
+import com.ecwid.consul.v1.ConsulClient;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.cloud.consul.ConsulProperties;
+
+/**
+ * @author Spencer Gibb
+ */
+public class ConsulPropertySourceTests {
+
+	private ConsulClient client;
+	private ConsulProperties properties;
+	private String prefix;
+	private String kvContext;
+	private String propertiesContext;
+
+	@Before
+	public void setup() {
+		properties = new ConsulProperties();
+		prefix = "consulPropertySourceTests" + new Random().nextInt(Integer.MAX_VALUE);
+		properties.setPrefix(prefix);
+		client = new ConsulClient(properties.getHost(), properties.getPort());
+	}
+
+	@After
+	public void teardown() {
+		client.deleteKVValues(prefix);
+	}
+
+	@Test
+	public void testKv() {
+		// key value properties
+		kvContext = prefix + "/kv";
+		client.setKVValue(kvContext + "/fooprop", "fookvval");
+		client.setKVValue(prefix+"/kv"+"/bar/prop", "barkvval");
+
+		ConsulPropertySource source = getConsulPropertySource(new ConsulConfigProperties(), kvContext);
+
+		assertProperties(source, "fookvval", "barkvval");
+	}
+
+	private void assertProperties(ConsulPropertySource source, String fooval, String barval) {
+		assertThat("fooprop was wrong", (String)source.getProperty("fooprop"), is(equalTo(fooval)));
+		assertThat("bar.prop was wrong", (String)source.getProperty("bar.prop"), is(equalTo(barval)));
+	}
+
+	@Test
+	public void testProperties() {
+		// properties file property
+		propertiesContext = prefix + "/properties";
+		client.setKVValue(propertiesContext+"/data", "fooprop=foopropval\nbar.prop=barpropval");
+
+		ConsulConfigProperties configProperties = new ConsulConfigProperties();
+		configProperties.setFormat(ConsulConfigProperties.Format.PROPERTIES);
+		ConsulPropertySource source = getConsulPropertySource(configProperties, propertiesContext);
+
+		assertProperties(source, "foopropval", "barpropval");
+	}
+
+	@Test
+	public void testYaml() {
+		// yaml file property
+		String yamlContext = prefix + "/yaml";
+		client.setKVValue(yamlContext+"/data", "fooprop: fooymlval\nbar:\n  prop: barymlval");
+
+		ConsulConfigProperties configProperties = new ConsulConfigProperties();
+		configProperties.setFormat(ConsulConfigProperties.Format.YAML);
+		ConsulPropertySource source = getConsulPropertySource(configProperties, yamlContext);
+
+		assertProperties(source, "fooymlval", "barymlval");
+	}
+
+	private ConsulPropertySource getConsulPropertySource(ConsulConfigProperties configProperties, String context) {
+		ConsulPropertySource source = new ConsulPropertySource(context, client, configProperties);
+		source.init();
+		String[] names = source.getPropertyNames();
+		assertThat("names was null", names, is(notNullValue()));
+		assertThat("names was wrong size", names.length, is(equalTo(2)));
+		return source;
+	}
+}
