6b6fd650eb63ff982948ab4b418f22cd212657e6
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2015-12-16 22:57:14 +0100

    Added missing scripts

---
+++ scripts/runAcceptanceTests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -o errexit
+
+SCRIPT_URL="https://raw.githubusercontent.com/spring-cloud-samples/brewery/master/acceptance-tests/scripts/runDockerAcceptanceTests.sh"
+AT_WHAT_TO_TEST="CONSUL"
+AT_VERSION="1.0.0.BUILD-SNAPSHOT"
+
+curl "${SCRIPT_URL}" --output runDockerAcceptanceTests.sh
+
+chmod +x runDockerAcceptanceTests.sh
+
+./runDockerAcceptanceTests.sh -t "${AT_WHAT_TO_TEST}" -v "${AT_VERSION}"
