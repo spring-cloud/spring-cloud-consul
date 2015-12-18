fca1ca69d4a6492ec829bf5c6009cf77dcfa6320
Marcin Grzejszczak <marcin@grzejszczak.pl>
2015-12-18 23:12:19 +0100

    Removed version param to use default BOM Brixton Snapshot version

--- scripts/runAcceptanceTests.sh
+++ scripts/runAcceptanceTests.sh
@@ -4,10 +4,9 @@ set -o errexit
 
 SCRIPT_URL="https://raw.githubusercontent.com/spring-cloud-samples/brewery/master/acceptance-tests/scripts/runDockerAcceptanceTests.sh"
 AT_WHAT_TO_TEST="CONSUL"
-AT_VERSION="1.0.0.BUILD-SNAPSHOT"
 
 curl "${SCRIPT_URL}" --output runDockerAcceptanceTests.sh
 
 chmod +x runDockerAcceptanceTests.sh
 
-./runDockerAcceptanceTests.sh -t "${AT_WHAT_TO_TEST}" -v "${AT_VERSION}"
+./runDockerAcceptanceTests.sh -t "${AT_WHAT_TO_TEST}"
