2072d6d57bae649b1e7f1aad6daa4eb30f3da7f0
Spencer Gibb <spencer@gibb.us>
2016-02-09 19:48:43 -0700

    fix problem with travis_run_consul.sh

--- src/test/bash/travis_run_consul.sh
+++ src/test/bash/travis_run_consul.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 BASEDIR=`dirname $0`/../../..
-${BASEDIR}/consul agent -server -bootstrap-expect 1 -advertise 127.0.0.1 -data-dir /tmp/consul -ui-dir ${BASEDIR}/src/test/resources/consul_ui #-config-dir ${BASEDIR}/src/test/resources/consul_config &
+${BASEDIR}/consul agent -server -bootstrap-expect 1 -advertise 127.0.0.1 -data-dir /tmp/consul -ui-dir ${BASEDIR}/src/test/resources/consul_ui & #-config-dir ${BASEDIR}/src/test/resources/consul_config &
 # wait for consul to elect a leader before sending acl
 #sleep 5
 #curl -X PUT -d @`dirname $0`/../../test/resources/consul_acl/consul_discovery_client_acl.json http://localhost:8500/v1/acl/create?token=2ee647bd-bd69-4118-9f34-b9a6e9e60746
