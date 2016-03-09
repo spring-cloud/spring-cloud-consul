b2e6f426d3bda6fa38b914d0956574f657d68206
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-03-09 13:24:13 +0100

    Added possibility to download consul without certificate check

--- src/main/bash/travis_install_consul.sh
+++ src/main/bash/travis_install_consul.sh
@@ -2,11 +2,19 @@
 
 CONSUL_VER="0.6.3"
 CONSUL_ZIP="consul_${CONSUL_VER}_linux_amd64.zip"
+IGNORE_CERTS="${IGNORE_CERTS:-no}"
+
 # cleanup
 rm "consul_*"
 rm "consul"
 # install consul
-wget "https://releases.hashicorp.com/consul/${CONSUL_VER}/${CONSUL_ZIP}"
+if [[ "${IGNORE_CERTS}" == "no" ]] ; then
+  echo "Downloading consul with certs verification"
+  wget "https://releases.hashicorp.com/consul/${CONSUL_VER}/${CONSUL_ZIP}"
+else
+  echo "WARNING... Downloading consul WITHOUT certs verification"
+  wget "https://releases.hashicorp.com/consul/${CONSUL_VER}/${CONSUL_ZIP}" --no-check-certificate
+fi
 unzip ${CONSUL_ZIP}
 # check
 ./consul --version
