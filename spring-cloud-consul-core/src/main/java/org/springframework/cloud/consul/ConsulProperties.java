d8b1bc7ed8804b95f1e005cfdcbfe531c689348b
Brian <asman.brian@gmail.com>
2017-03-23 14:18:18 -0400

    annotate consul config properties as validated (#291)

--- spring-cloud-consul-core/src/main/java/org/springframework/cloud/consul/ConsulProperties.java
+++ spring-cloud-consul-core/src/main/java/org/springframework/cloud/consul/ConsulProperties.java
@@ -21,12 +21,14 @@ import javax.validation.constraints.NotNull;
 import lombok.Data;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 /**
  * @author Spencer Gibb
  */
 @ConfigurationProperties("spring.cloud.consul")
 @Data
+@Validated
 public class ConsulProperties {
 	/** Consul agent hostname. Defaults to 'localhost'. */
 	@NotNull
