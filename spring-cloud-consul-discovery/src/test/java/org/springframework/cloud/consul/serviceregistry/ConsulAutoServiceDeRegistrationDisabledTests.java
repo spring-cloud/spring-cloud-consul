b41d72bacba3878a32ec5b716e4262e0b43c77a7
jonfreedman <jon.freedman@zoho.com>
2017-06-23 10:37:25 +0100

    update copyright/author

--- spring-cloud-consul-discovery/src/test/java/org/springframework/cloud/consul/serviceregistry/ConsulAutoServiceDeRegistrationDisabledTests.java
+++ spring-cloud-consul-discovery/src/test/java/org/springframework/cloud/consul/serviceregistry/ConsulAutoServiceDeRegistrationDisabledTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2016 the original author or authors.
+ * Copyright 2013-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -38,7 +38,7 @@ import static org.junit.Assert.assertNull;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 /**
- * @author Spencer Gibb
+ * @author Jon Freedman
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ConsulAutoServiceRegistrationDisabledTests.TestConfig.class,
