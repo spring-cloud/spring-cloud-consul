467c4abe2969e5f7d5d921e7bcfb04bd4e576811
Spencer Gibb <spencer@gibb.us>
2018-05-16 16:21:38 -0400

    debug circleci

--- src/test/bash/travis_run_consul.sh
+++ src/test/bash/travis_run_consul.sh
@@ -6,4 +6,6 @@ ${BASEDIR}/consul agent -server -bootstrap-expect 1 -advertise 127.0.0.1 -data-d
 # wait for consul to elect a leader before sending acl
 sleep 5
 curl -X PUT -d @`dirname $0`/../../test/resources/consul_acl/consul_anonymous_acl.json http://localhost:8500/v1/acl/create?token=2ee647bd-bd69-4118-9f34-b9a6e9e60746
+echo "consul_anonymous_acl installed"
 curl -X PUT -d @`dirname $0`/../../test/resources/consul_acl/consul_discovery_client_acl.json http://localhost:8500/v1/acl/create?token=2ee647bd-bd69-4118-9f34-b9a6e9e60746
+echo "consul_discovery_client_acl installed"
