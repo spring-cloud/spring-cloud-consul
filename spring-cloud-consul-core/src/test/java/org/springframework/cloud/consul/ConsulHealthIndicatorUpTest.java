f8e7cb3ad69ed9a042cb7080704d575f21f67256
Lomesh Patel <lomesh.patel@gmail.com>
2018-09-19 19:50:56 -0400

    Log Exception from ConsulHealthIndicator
    - Removed `try...catch` block from `ConsulHealthIndicator` to let
    `AbstractHealthIndicator` log and handle the `Exception` appropriately
    - Added tests for UP and DOWN status
    
    Fixes gh-447

---
+++ spring-cloud-consul-core/src/test/java/org/springframework/cloud/consul/ConsulHealthIndicatorUpTest.java
@@ -0,0 +1,35 @@
+package org.springframework.cloud.consul;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.actuate.health.HealthEndpoint;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Lomesh Patel (lomeshpatel)
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConsulHealthIndicatorUpTest {
+
+	@Autowired
+	private HealthEndpoint healthEndpoint;
+
+	@Test
+	public void doHealthCheck() {
+		assertEquals("health status was not UP", Status.UP,
+				healthEndpoint.health().getStatus());
+	}
+
+	@EnableAutoConfiguration
+	@SpringBootConfiguration
+	protected static class TestConfig {
+	}
+}
