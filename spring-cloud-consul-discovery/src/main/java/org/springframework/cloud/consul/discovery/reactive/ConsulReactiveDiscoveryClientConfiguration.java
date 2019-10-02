ef0b356d97f84afa5e893e400ab0c151a44e91b0
Tim Ysewyn <tysewyn@pivotal.io>
2019-10-02 16:40:34 +0200

    Added support for reactive service discovery

---
+++ spring-cloud-consul-discovery/src/main/java/org/springframework/cloud/consul/discovery/reactive/ConsulReactiveDiscoveryClientConfiguration.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2013-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.consul.discovery.reactive;
+
+import com.ecwid.consul.v1.ConsulClient;
+
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.ConditionalOnDiscoveryEnabled;
+import org.springframework.cloud.client.ConditionalOnDiscoveryHealthIndicatorEnabled;
+import org.springframework.cloud.client.ConditionalOnReactiveDiscoveryEnabled;
+import org.springframework.cloud.client.ReactiveCommonsClientAutoConfiguration;
+import org.springframework.cloud.client.discovery.composite.reactive.ReactiveCompositeDiscoveryClientAutoConfiguration;
+import org.springframework.cloud.client.discovery.health.DiscoveryClientHealthIndicatorProperties;
+import org.springframework.cloud.client.discovery.health.reactive.ReactiveDiscoveryClientHealthIndicator;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.commons.util.UtilAutoConfiguration;
+import org.springframework.cloud.consul.ConditionalOnConsulEnabled;
+import org.springframework.cloud.consul.ConsulAutoConfiguration;
+import org.springframework.cloud.consul.discovery.ConditionalOnConsulDiscoveryEnabled;
+import org.springframework.cloud.consul.discovery.ConsulDiscoveryProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Tim Ysewyn
+ */
+@Configuration
+@ConditionalOnDiscoveryEnabled
+@ConditionalOnReactiveDiscoveryEnabled
+@ConditionalOnConsulEnabled
+@ConditionalOnConsulDiscoveryEnabled
+@EnableConfigurationProperties(DiscoveryClientHealthIndicatorProperties.class)
+@AutoConfigureBefore(ReactiveCommonsClientAutoConfiguration.class)
+@AutoConfigureAfter({ UtilAutoConfiguration.class,
+		ReactiveCompositeDiscoveryClientAutoConfiguration.class,
+		ConsulAutoConfiguration.class })
+public class ConsulReactiveDiscoveryClientConfiguration {
+
+	@Bean
+	@ConditionalOnMissingBean
+	public ConsulDiscoveryProperties consulDiscoveryProperties(InetUtils inetUtils) {
+		return new ConsulDiscoveryProperties(inetUtils);
+	}
+
+	@Bean
+	@ConditionalOnMissingBean
+	public ConsulReactiveDiscoveryClient consulReactiveDiscoveryClient(
+			ConsulClient client, ConsulDiscoveryProperties discoveryProperties) {
+		return new ConsulReactiveDiscoveryClient(client, discoveryProperties);
+	}
+
+	@Bean
+	@ConditionalOnClass(
+			name = "org.springframework.boot.actuate.health.ReactiveHealthIndicator")
+	@ConditionalOnDiscoveryHealthIndicatorEnabled
+	public ReactiveDiscoveryClientHealthIndicator consulReactiveDiscoveryClientHealthIndicator(
+			ConsulReactiveDiscoveryClient client,
+			DiscoveryClientHealthIndicatorProperties properties) {
+		return new ReactiveDiscoveryClientHealthIndicator(client, properties);
+	}
+
+}
