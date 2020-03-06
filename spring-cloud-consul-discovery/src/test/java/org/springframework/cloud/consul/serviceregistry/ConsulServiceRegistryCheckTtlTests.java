6afc98ef535ddab6a3da964ca4ad840343c29a8f
Spencer Gibb <spencer@gibb.us>
2020-03-06 11:25:39 -0500

    Initial test containers support

--- spring-cloud-consul-discovery/src/test/java/org/springframework/cloud/consul/serviceregistry/ConsulServiceRegistryCheckTtlTests.java
+++ spring-cloud-consul-discovery/src/test/java/org/springframework/cloud/consul/serviceregistry/ConsulServiceRegistryCheckTtlTests.java
@@ -20,8 +20,15 @@ import java.lang.reflect.Field;
 import java.util.Map;
 
 import com.ecwid.consul.v1.agent.model.NewService;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.containers.wait.strategy.Wait;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -49,8 +56,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ConsulServiceRegistryCheckTtlTests {
 
+	static final Logger logger = LoggerFactory.getLogger(ConsulServiceRegistryTests.class);
+
+	public static GenericContainer consul = new GenericContainer("consul:1.7.1")
+		.waitingFor(Wait.forHttp("/v1/status/leader"))
+		.withExposedPorts(8500)
+		.withCommand("agent", "-dev", "-server", "-bootstrap", "-client", "0.0.0.0", "-log-level", "trace");
+
 	@LocalServerPort
-	private int randomServerPort;
+	private int port;
+
+	@BeforeClass
+	public static void before() {
+		consul.start();
+		consul.followOutput(new Slf4jLogConsumer(logger).withSeparateOutputStreams());
+		Integer mappedPort = consul.getMappedPort(8500);
+		System.setProperty("spring.cloud.consul.port", String.valueOf(mappedPort));
+	}
+
+	@AfterClass
+	public static void after() {
+		System.clearProperty("spring.cloud.consul.port");
+		consul.stop();
+	}
 
 	@Autowired
 	private ConsulDiscoveryProperties discoveryProperties;
@@ -69,7 +97,7 @@ public class ConsulServiceRegistryCheckTtlTests {
 		NewService.Check httpCheck = new NewService.Check();
 		httpCheck.setHttp(
 				String.format("%s://%s:%s%s", this.discoveryProperties.getScheme(),
-						this.discoveryProperties.getHostname(), this.randomServerPort,
+						this.discoveryProperties.getHostname(), this.port,
 						this.discoveryProperties.getHealthCheckPath()));
 		httpCheck.setInterval(this.discoveryProperties.getHealthCheckInterval());
 		NewService httpService = new NewService();
