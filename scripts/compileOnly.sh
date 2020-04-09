ce0ac8b2bea852ad7ad5d4c3f60ed7f95fa9a007
Spencer Gibb <spencer@gibb.us>
2020-04-09 01:06:26 -0400

    Updates consul to 1.7.2
    fixes gh-491

--- scripts/compileOnly.sh
+++ scripts/compileOnly.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./src/main/bash/travis_install_consul.sh
-./src/test/bash/travis_run_consul.sh
+./src/main/bash/ci_install_consul.sh
+./src/test/bash/ci_run_consul.sh
 ./mvnw clean install -B -Pdocs -DskipTests -fae
