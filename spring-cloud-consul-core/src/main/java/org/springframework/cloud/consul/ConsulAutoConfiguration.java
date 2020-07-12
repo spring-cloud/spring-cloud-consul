4a6d7027f04f40e2dfe0aaa2a3f5bc6373e37e30
bono007 <chris.bono@gmail.com>
2020-07-12 09:55:13 -0500

    Allow ConsulHealthIndicator to bypass the services query
    Fixes gh-646

--- spring-cloud-consul-core/src/main/java/org/springframework/cloud/consul/ConsulAutoConfiguration.java
+++ spring-cloud-consul-core/src/main/java/org/springframework/cloud/consul/ConsulAutoConfiguration.java
@@ -71,6 +71,7 @@ public class ConsulAutoConfiguration {
 
 	@Configuration(proxyBeanMethods = false)
 	@ConditionalOnClass(Endpoint.class)
+	@EnableConfigurationProperties(ConsulHealthIndicatorProperties.class)
 	protected static class ConsulHealthConfig {
 
 		@Bean
@@ -83,8 +84,9 @@ public class ConsulAutoConfiguration {
 		@Bean
 		@ConditionalOnMissingBean
 		@ConditionalOnEnabledHealthIndicator("consul")
-		public ConsulHealthIndicator consulHealthIndicator(ConsulClient consulClient) {
-			return new ConsulHealthIndicator(consulClient);
+		public ConsulHealthIndicator consulHealthIndicator(ConsulClient consulClient,
+				ConsulHealthIndicatorProperties properties) {
+			return new ConsulHealthIndicator(consulClient, properties);
 		}
 
 	}
