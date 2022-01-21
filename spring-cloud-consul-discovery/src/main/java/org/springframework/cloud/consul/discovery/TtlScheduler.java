eb47c1a614689e2ea99af5926ed10be6411edc2d
陈金凯 <chenjinkai@ceair.com>
2022-01-21 14:49:44 +0800

    add my name

--- spring-cloud-consul-discovery/src/main/java/org/springframework/cloud/consul/discovery/TtlScheduler.java
+++ spring-cloud-consul-discovery/src/main/java/org/springframework/cloud/consul/discovery/TtlScheduler.java
@@ -34,6 +34,7 @@ import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
  * Created by nicu on 11.03.2015.
  *
  * @author Stéphane LEROY
+ * @author jinkai.chen
  */
 public class TtlScheduler {
 
