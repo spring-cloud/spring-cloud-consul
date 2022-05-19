152171b135d689df8c341d897fbc3e2a75a7da23
Daniel Wu <daniel.wu06@sap.com>
2022-05-19 10:48:33 +0800

    allow key deletion to trigger configuration refresh

--- spring-cloud-consul-config/src/main/java/org/springframework/cloud/consul/config/ConfigWatch.java
+++ spring-cloud-consul-config/src/main/java/org/springframework/cloud/consul/config/ConfigWatch.java
@@ -157,34 +157,26 @@ public class ConfigWatch implements ApplicationEventPublisherAware, SmartLifecyc
 				Response<List<GetValue>> response = this.consul.getKVValues(context, aclToken,
 						new QueryParams(this.properties.getWatch().getWaitTime(), currentIndex));
 
-				// if response.value == null, response was a 404, otherwise it was a
-				// 200, reducing churn if there wasn't anything
-				if (response.getValue() != null && !response.getValue().isEmpty()) {
-					Long newIndex = response.getConsulIndex();
-
-					if (newIndex != null && !newIndex.equals(currentIndex)) {
-						// don't publish the same index again, don't publish the first
-						// time (-1) so index can be primed
-						if (!this.consulIndexes.containsValue(newIndex) && !currentIndex.equals(-1L)) {
-							if (log.isTraceEnabled()) {
-								log.trace("Context " + context + " has new index " + newIndex);
-							}
-							RefreshEventData data = new RefreshEventData(context, currentIndex, newIndex);
-							this.publisher.publishEvent(new RefreshEvent(this, data, data.toString()));
-						}
-						else if (log.isTraceEnabled()) {
-							log.trace("Event for index already published for context " + context);
-						}
-						this.consulIndexes.put(context, newIndex);
-					}
-					else if (log.isTraceEnabled()) {
-						log.trace("Same index for context " + context);
-					}
-				}
-				else if (log.isTraceEnabled()) {
-					log.trace("No value for context " + context);
-				}
-
+				// if response.value == null, response was a 404, a key is deleted
+				 Long newIndex = response.getConsulIndex();
+				 if (newIndex != null && !newIndex.equals(currentIndex)) {
+					  // don't publish the same index again, don't publish the first
+					  // time (-1) so index can be primed
+					  if (!this.consulIndexes.containsValue(newIndex) && !currentIndex.equals(-1L)) {
+						  if (log.isTraceEnabled()) {
+							  log.trace("Context " + context + " has new index " + newIndex);
+						  }
+						  RefreshEventData data = new RefreshEventData(context, currentIndex, newIndex);
+						  this.publisher.publishEvent(new RefreshEvent(this, data, data.toString()));
+					  }
+					  else if (log.isTraceEnabled()) {
+						  log.trace("Event for index already published for context " + context);
+					  }
+					  this.consulIndexes.put(context, newIndex);
+				 }
+				 else if (log.isTraceEnabled()) {
+					  log.trace("Same index for context " + context);
+				 }
 			}
 			catch (Exception e) {
 				// only fail fast on the initial query, otherwise just log the error
