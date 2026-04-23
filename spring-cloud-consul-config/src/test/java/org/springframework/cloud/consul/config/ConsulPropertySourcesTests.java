6d98be42e27a09f902beb8b8b7eb0b57867aaa7e
Ryan Baxter <ryan.baxter@broadcom.com>
2026-04-23 09:16:34 -0400

    Fix compile errors after merge

--- spring-cloud-consul-config/src/test/java/org/springframework/cloud/consul/config/ConsulPropertySourcesTests.java
+++ spring-cloud-consul-config/src/test/java/org/springframework/cloud/consul/config/ConsulPropertySourcesTests.java
@@ -20,15 +20,16 @@ import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 
-import com.ecwid.consul.v1.ConsulClient;
-import com.ecwid.consul.v1.QueryParams;
-import com.ecwid.consul.v1.Response;
-import com.ecwid.consul.v1.kv.model.GetValue;
 import org.apache.commons.logging.LogFactory;
 import org.junit.Test;
 
+import org.springframework.cloud.consul.ConsulClient;
+import org.springframework.cloud.consul.model.http.kv.GetValue;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.nullable;
 import static org.mockito.Mockito.mock;
@@ -48,8 +49,8 @@ public class ConsulPropertySourcesTests {
 	@Test
 	public void createPropertySourceStoresNormalizedContextInIndex() {
 		ConsulClient consul = mock(ConsulClient.class);
-		Response<List<GetValue>> response = new Response<>(Collections.emptyList(), 1L, false, 1L);
-		when(consul.getKVValues(eq("test/"), nullable(String.class), any(QueryParams.class))).thenReturn(response);
+		ResponseEntity<List<GetValue>> response = new ResponseEntity<List<GetValue>>(HttpStatus.OK);
+		when(consul.getKVValues(eq("test/"), nullable(String.class))).thenReturn(response);
 
 		ConsulConfigProperties properties = new ConsulConfigProperties();
 		ConsulPropertySources sources = new ConsulPropertySources(properties,
@@ -65,7 +66,10 @@ public class ConsulPropertySourcesTests {
 	@Test
 	public void createPropertySourceFilesFormatStoresNormalizedContextInIndex() {
 		ConsulClient consul = mock(ConsulClient.class);
-		Response<GetValue> response = new Response<>(null, 1L, false, 1L);
+		ResponseEntity<List<GetValue>> response = mock(ResponseEntity.class);
+		when(response.getStatusCode()).thenReturn(HttpStatus.OK);
+		when(response.getBody()).thenReturn(Collections.emptyList());
+		when(response.getHeaders()).thenReturn(new HttpHeaders());
 		when(consul.getKVValue(eq("test.yml"), nullable(String.class))).thenReturn(response);
 
 		ConsulConfigProperties properties = new ConsulConfigProperties();
